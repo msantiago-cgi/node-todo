1f89b7aa377699b02a57a61aa7940daf8493c8dc
msantiago-cgi <msantiago@cgiinteractive.com>
2015-08-19 15:41:49 -0400

    changed the db url to use ec2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
 	// the database url to connect
-	url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
+	url : 'mongodb://msantiagoAdmin:125612@ec2-52-20-76-93.compute-1.amazonaws.com:27017/dummyDB'
 }
